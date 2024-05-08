1c6ae489f6f386b8eb9e7e107f20dad591987d3d
Sky <79532409+Bormey-Sky@users.noreply.github.com>
2024-05-08 16:14:15 +0700

    added train and evaluate

--- kss/train.py
+++ kss/train.py
@@ -1,35 +1,37 @@
 import argparse
 import os
+import pandas as pd
 from utils import read_config_file
 from models.tfidf import TFIDF
+from models.fasttext import FASTTEXT
 
 parser = argparse.ArgumentParser(description='Run the training loop.')
 parser.add_argument('config', type=str, help='Path to config file.')
 parser.add_argument('type', type=str, help="The type of architecture to use.", choices=['tfidf','fasttext', 'bert'])
 parser.add_argument('dataset_path', type=str,
-                    help='Path to dataset. (text file if of type khpos or directory if of type phylypo)')
+                    help='Path to dataset csv file. (text file if of type khpos or directory if of type phylypo)')
+parser.add_argument('--data_dir', type=str,
+                    help='Path to dataset. (text file if of type khpos or directory if of type phylypo)', default="./datasets")
 parser.add_argument('--output_dir', type=str,
                     help='Path to output directory.', default=".")
+parser.add_argument('--model_ext', type=str,
+                    help='Path to output directory.', default="pkl")
 args = parser.parse_args()
 
-print(args)
-
-
 config = read_config_file(args.config)
+dataset_path = os.path.join(args.data_dir, args.dataset_path)
+datasets = pd.read_csv(dataset_path)
 
-pretrained_weight_path = os.path.join("pretrained", config['pretrained_weights'])
-
-print(pretrained_weight_path)
-
-exit()
+datasets["source"] = args.data_dir + "/documents/" + datasets["source"]
+datasets["target"] = args.data_dir + "/documents/" + datasets["target"]
 
 model = None
 if args.type == "tfidf":
     model = TFIDF(config)
 elif args.type == "fasttext":
-    pass
+    model = FASTTEXT(config)
 
-model.train()
+model.train(datasets)
 
-
-print(config)
+output_path = os.path.join(args.output_dir, os.path.basename(args.config).replace(".json", f".{args.model_ext}"))
+model.save(output_path=output_path)
